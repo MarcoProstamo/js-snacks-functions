bdc4903ffe6f61221163db4b06c703521a6c0267
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-21 14:01:31 +0200

    Solved → 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,6 +4,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const initials = (names) => {
+  const initialsArray = [];
+  names.forEach((name) => {
+    initialsArray.push(name[0]);
+  });
+  return initialsArray;
+};
+
 // Invoca la funzione qui e stampa il risultato in console
+console.log(initials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
