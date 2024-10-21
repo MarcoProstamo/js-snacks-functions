91e7589588e574d3677a5dc932f835f4a4bf2c97
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-21 14:11:20 +0200

    Solved → 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,21 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+const vocalsCounter = (string) => {
+  let vocalsCounter = 0;
+  string = string.toLowerCase();
+  string = string.split("");
+  string.forEach((char) => {
+    char === "a" || char === "e" || char === "i" || char === "o" || char === "u"
+      ? vocalsCounter++
+      : null;
+  });
+  return vocalsCounter;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(vocalsCounter(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
