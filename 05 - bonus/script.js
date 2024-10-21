47963ebcbccfae973a88630cb0ae7c21a540c431
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-21 14:33:03 +0200

    Solved → 5

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,21 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const name = "Mario";
 
+const fullDate = new Date();
+const hour = fullDate.getHours();
 
 // Dichiara la funzione qui.
-
+const greeting = (name) => {
+  let greeting;
+  if (hour <= 13) greeting = "Buongiorno";
+  if (hour > 13 && hour <= 17) greeting = "Buon pomeriggio";
+  if (hour > 17) greeting = "Buonasera";
+  return `${greeting} ${name}`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(greeting(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
