928425ab1f201c31aa9fb94d328bd91adfe3599c
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-21 14:13:40 +0200

    Solved → 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,13 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
+const greeting = (name) => {
+  return `Ciao ${name}`;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(greeting(name));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
